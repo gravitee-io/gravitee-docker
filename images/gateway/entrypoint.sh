87413ad3caf3ac42c61998c93a6d8c9d6ce64a7c
Nicolas Géraud <nicolas.geraud@gmail.com>
2015-09-13 17:02:35 +0200

    feat(reporter-es): configuration

--- images/gateway/entrypoint.sh
+++ images/gateway/entrypoint.sh
@@ -7,6 +7,8 @@ setup() {
         rm /home/gravitee/config/gravitee.yml
         cp /home/gravitee/config/local_default_gravitee.yml /home/gravitee/config/gravitee.yml
     fi
+    echo "Configure elasticsearch address from ${ES_DEFAULT_HOST} to ${ES_HOST}"
+    sed -i s/"'${ES_DEFAULT_HOST}'"/"'${ES_HOST}'"/ /home/gravitee/config/gravitee.yml
 
     echo "Configure mongodb address from ${MONGO_DEFAULT_HOST}:${MONGO_DEFAULT_PORT} to ${MONGO_HOST}:${MONGO_PORT}"
     sed -i s/"host: ${MONGO_DEFAULT_HOST}"/"host: ${MONGO_HOST}"/ /home/gravitee/config/gravitee.yml
