6d344c47180f99ad74b41532cf638cb85b3fda72
Nicolas Géraud <nicolas.geraud@gmail.com>
2016-05-24 18:01:06 +0200

    feat(): update docker images (#4)
    - create a new Jenkins image fo our CI server
     - use elasticsearch and mongodb official images
     - create a nightly docker-compose
     - use docker4mac and docker4windows for the demo launcher
     - fix bugs
    
    fix #1

--- images/kibana3/healthcheck.sh
+++
@@ -1,5 +0,0 @@
-healthcheck() {
-    local HC_HOST=$1 HC_PORT=$2 HC_SLEEP_IN_SECONDS=$3
-    echo "healthcheck $HC_HOST:$HC_PORT (wait ${HC_SLEEP_IN_SECONDS}s)"
-    while ! nc -z -w 2 $HC_HOST $HC_PORT; do echo "$HC_HOST:$HC_PORT not reachable. Try again in ${HC_SLEEP_IN_SECONDS}s" && sleep $HC_SLEEP_IN_SECONDS; done
-}
--- images/management-ui/entrypoint.sh
+++ images/management-ui/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 setup() {
-    echo "Configure management api url from ${MGMT_API_DEFAULT_HOST}:${MGMT_API_DEFAULT_PORT} to ${MGMT_API_HOST}:${MGMT_API_PORT}"
-    sed -i s/"${MGMT_API_DEFAULT_HOST}:${MGMT_API_DEFAULT_PORT}"/"${MGMT_API_HOST}:${MGMT_API_PORT}"/ /var/www/html/scripts/app*.js
+    echo "Configure management api url to ${MGMT_API_URL}"
+    sed -i s/"'\/management\/'"/"'${MGMT_API_URL}'"/ /var/www/html/constants.js
 }
 
 setup
