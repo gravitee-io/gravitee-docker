ef00ac64c32ff1915b58287e4464d8c8d80e147d
Nicolas Géraud <nicolas.geraud@gmail.com>
2016-06-16 00:17:15 +0200

    fix(gateway): fix demo host/port for the gateway

--- images/gateway/entrypoint.sh
+++ images/gateway/entrypoint.sh
@@ -3,12 +3,12 @@
 setup() {
 
     echo "Configure elasticsearch address from ${ES_DEFAULT_HOST}:${ES_DEFAULT_PORT} to ${_HOST}:${_PORT}"
-    sed -i 162s/"host: ${ES_DEFAULT_HOST}"/"host: ${ES_HOST}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
-    sed -i 163s/"port: ${ES_DEFAULT_PORT}"/"port: ${ES_PORT}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
+    sed -i 159s/"host: ${ES_DEFAULT_HOST}"/"host: ${ES_HOST}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
+    sed -i 160s/"port: ${ES_DEFAULT_PORT}"/"port: ${ES_PORT}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
 
     echo "Configure mongodb address from ${MONGO_DEFAULT_HOST}:${MONGO_DEFAULT_PORT} to ${MONGO_HOST}:${MONGO_PORT}"
-    sed -i 159s/"host: ${MONGO_DEFAULT_HOST}"/"host: ${MONGO_HOST}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
-    sed -i 160s/"port: ${MONGO_DEFAULT_PORT}"/"port: ${MONGO_PORT}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
+    sed -i 156s/"host: ${MONGO_DEFAULT_HOST}"/"host: ${MONGO_HOST}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
+    sed -i 157s/"port: ${MONGO_DEFAULT_PORT}"/"port: ${MONGO_PORT}"/ ${GRAVITEEIO_HOME}/config/gravitee.yml
 }
 
 if [[ "true" == "$check_links" ]]; then
