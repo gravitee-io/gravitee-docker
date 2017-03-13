8a1a55b3e660c0cf69661058ef745f5032db7a5d
Nicolas Géraud <nicolas.geraud@gmail.com>
2017-03-13 11:11:35 +0100

    chore: Migration to typescript and angularJS 1.5
    Closes gravitee-io/issues#483

--- images/management-ui/files/entrypoint.sh
+++ images/management-ui/files/entrypoint.sh
@@ -2,8 +2,8 @@
 
 setup() {
     echo "Configure management api url to ${MGMT_API_URL}"
-    cat /var/www/html/constants.js.template | \
-    sed "s#/management/#${MGMT_API_URL}#g" > /var/www/html/constants.js
+    cat /var/www/html/constants.json.template | \
+    sed "s#/management/#${MGMT_API_URL}#g" > /var/www/html/constants.json
 }
 
 setup
