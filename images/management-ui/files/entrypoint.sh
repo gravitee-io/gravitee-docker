bd4e1d7834adbcf6d6c3e5be5a7d7d1a153e1be7
aelamrani <azize.elamrani@gmail.com>
2017-10-10 10:11:33 +0200

    feat: Replace the default rest api base url correctly

--- images/management-ui/files/entrypoint.sh
+++ images/management-ui/files/entrypoint.sh
@@ -3,7 +3,7 @@
 setup() {
     echo "Configure management api url to ${MGMT_API_URL}"
     cat /var/www/html/constants.json.template | \
-    sed "s#/management/#${MGMT_API_URL}#g" > /var/www/html/constants.json
+    sed "s#http://localhost:8083/management/#${MGMT_API_URL}#g" > /var/www/html/constants.json
 }
 
 setup
