f1317a8e36fea21e1a5279ededa993879e926e1d
Nicolas Géraud <nicolas.geraud@gmail.com>
2018-04-12 11:29:05 +0200

    feat(confd): Initial Implementation

--- images/management-ui/files/entrypoint.sh
+++ images/management-ui/files/entrypoint.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/bin/sh
+echo "CONFD_BACKEND : ${CONFD_BACKEND}"
+echo "CONFD_NODE    : ${CONFD_NODE}"
+echo "CONFD_PREFIX  : ${CONFD_PREFIX}"
 
-setup() {
-    echo "Configure management api url to ${MGMT_API_URL}"
-    cat /var/www/html/constants.json.template | \
-    sed "s#http://localhost:8083/management/#${MGMT_API_URL}#g" > /var/www/html/constants.json
-}
-
-setup
+if [ ! -z $CONFD_NODE ]
+then
+    mv /var/www/html/constants.json /constants.json.ori
+    if [ ! -z $CONFD_NODE ]
+    then
+        confd -onetime -backend ${CONFD_BACKEND} -node ${CONFD_NODE} -prefix="${CONFD_PREFIX}" -log-level="debug"
+    else
+        confd -onetime -backend ${CONFD_BACKEND} -node ${CONFD_NODE} -log-level="debug"
+    fi
+fi
+echo "Gravitee.io APIM Webui is ready."
 exec "$@"
