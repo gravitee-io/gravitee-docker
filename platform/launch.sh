e90b9d01a04d2219b351490a7eec970b09356894
jhaeyaert <jeoffrey.haeyaert@gmail.com>
2020-01-06 15:39:06 +0100

    feat(nightly): Adapt docker-compose and launch script to run any Gravitee version (#39)

--- platform/launch.sh
+++ platform/launch.sh
@@ -16,8 +16,8 @@
 #
 
 readonly WORKDIR="$HOME/graviteeio-api-platform"
-readonly DIRNAME=`dirname $0`
-readonly PROGNAME=`basename $0`
+readonly DIRNAME=$(dirname $0)
+readonly PROGNAME=$(basename $0)
 readonly color_title='\033[32m'
 readonly color_text='\033[1;36m'
 
@@ -26,6 +26,55 @@ declare cygwin=false
 declare darwin=false
 declare linux=false
 declare dc_exec="docker-compose up"
+declare apim_version="latest"
+declare am_version="latest"
+declare am_mgt_api_base=""
+
+optspec=":h-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+    -)
+        case "${OPTARG}" in
+        am_version=*)
+            am_version=${OPTARG#*=}
+            ;;
+        apim_version=*)
+            apim_version=${OPTARG#*=}
+            ;;
+        version=*)
+            am_version=${OPTARG#*=}
+            apim_version=${OPTARG#*=}
+            ;;
+        *)
+            if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                echo "Unknown option --${OPTARG}" >&2
+            fi
+            ;;
+        esac
+        ;;
+    h)
+        echo "usage: $0 [-v] [--version[=]<value>]" >&2
+        echo "usage: $0 [-v] [--am_version[=]<value>] [--apim_version[=]<value>]" >&2
+
+        exit 2
+        ;;
+    *)
+        if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+            echo "Non-option argument: '-${OPTARG}'" >&2
+        fi
+        ;;
+    esac
+done
+
+echo "Running Gravitee platform"
+echo "APIM version: ${apim_version}"
+echo "AM version: ${am_version}"
+
+if [[ $am_version == nightly ]] || [[ $am_version == 3.* ]]; then
+    am_mgt_api_base="\/"
+fi
+
+dc_exec="APIM_VERSION=${apim_version} AM_VERSION=${am_version} AM_MGT_API_BASE=${am_mgt_api_base} ${dc_exec}"
 
 welcome() {
     echo
@@ -35,7 +84,7 @@ welcome() {
     echo -e " ${color_title} | | |_ |  __/ _  \ \ / / | __/ _ \/ _ \ | |/ _ \                           \033[0m"
     echo -e " ${color_title} | |__| | | | (_| |\ V /| | ||  __/  __/_| | (_) |                          \033[0m"
     echo -e " ${color_title}  \_____|_|  \__,_| \_/ |_|\__\___|\___(_)_|\___/                           \033[0m"
-    echo -e " ${color_title}                         \033[0m${color_text}http://gravitee.io\033[0m"     
+    echo -e " ${color_title}                         \033[0m${color_text}http://gravitee.io\033[0m"
     echo -e " ${color_title}                                                                            \033[0m"
     echo -e " ${color_title}                 _____ _____    _____  _       _    __                      \033[0m"
     echo -e " ${color_title}           /\   |  __ \_   _|  |  __ \| |     | |  / _|                     \033[0m"
@@ -51,30 +100,31 @@ welcome() {
 init_env() {
     local dockergrp
     # define env
-    case "`uname`" in
-        CYGWIN*)
-            cygwin=true
-            ;;
+    case "$(uname)" in
+    CYGWIN*)
+        cygwin=true
+        ;;
 
-        Darwin*)
-            darwin=true
-            ;;
+    Darwin*)
+        darwin=true
+        ;;
 
-        Linux)
-            linux=true
-            ;;
+    Linux)
+        linux=true
+        ;;
     esac
 
     # test if docker must be run with sudo
     dockergrp=$(groups | grep -c docker)
     if [[ $darwin == false && $dockergrp == 0 ]]; then
-        dc_exec="sudo $dc_exec";
+        dc_exec="sudo $dc_exec"
     fi
 }
 
 main() {
     welcome
     init_env
+
     if [[ $? != 0 ]]; then
         exit 1
     fi
@@ -83,15 +133,14 @@ main() {
     pushd $WORKDIR > /dev/null
         echo "Download required files ..."
         mkdir -p traefik/certs ; mkdir -p traefik/config ; mkdir -p mongo/docker-entrypoint-initdb.d
-        curl -L https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/docker-compose.yml -o "docker-compose.yml"
-        cd mongo/docker-entrypoint-initdb.d && { curl -L https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/mongo/docker-entrypoint-initdb.d/create-index.js -o "create-index.js" ; cd -; }
-        cd traefik/certs && { curl -L https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/certs/gio-selfsigned.crt -o "gio-selfsigned.crt" ; cd -; }
-        cd traefik/certs && { curl -L https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/certs/gio-selfsigned.key -o "gio-selfsigned.key" ; cd -; }
-        cd traefik/certs && { curl -L https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/certs/gio.pem -o "gio.pem" ; cd -; }
-        cd traefik/config && { curl -L https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/config/traefik.toml -o "traefik.toml" ; cd -; }
+        curl -Lf https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/docker-compose.yml -o "docker-compose.yml"
+        cd mongo/docker-entrypoint-initdb.d && { curl -Lf https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/mongo/docker-entrypoint-initdb.d/create-index.js -o "create-index.js" ; cd -; }
+        cd traefik/certs && { curl -Lf https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/certs/gio-selfsigned.crt -o "gio-selfsigned.crt" ; cd -; }
+        cd traefik/certs && { curl -Lf https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/certs/gio-selfsigned.key -o "gio-selfsigned.key" ; cd -; }
+        cd traefik/config && { curl -Lf https://raw.githubusercontent.com/gravitee-io/gravitee-docker/master/platform/traefik/config/traefik.toml -o "traefik.toml" ; cd -; }
         echo
         echo "Launch Gravitee.io API Platform..."
-        $dc_exec
+        eval ${dc_exec}
     popd > /dev/null
 }
 
