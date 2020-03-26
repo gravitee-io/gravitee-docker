2cd006dd1072ce20eec653ef9b4f39c39d700c06
Azize Elamrani <azize.elamrani@gmail.com>
2020-03-26 21:12:23 +0100

    chore: Remove portal api

--- images/portal-ui/files/entrypoint.sh
+++ images/portal-ui/files/entrypoint.sh
@@ -3,7 +3,7 @@
 setup() {
     echo "Configure portal api url to ${PORTAL_API_URL}"
     cat /var/www/html/assets/config.json.template | \
-    sed "s#/portal/DEFAULT#${PORTAL_API_URL}#g" > /var/www/html/assets/config.json
+    sed "s#/portal/environments/DEFAULT#${PORTAL_API_URL}#g" > /var/www/html/assets/config.json
 }
 
 setup
