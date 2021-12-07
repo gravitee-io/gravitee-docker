4e87e884252b57d290693841e66ba0b518f8c508
Yann Tavernier <yann.tavernier@graviteesource.com>
2021-12-07 16:28:20 +0100

    chore: add cloud-app

---
+++ platform/3.x/saml/authsources.php
@@ -0,0 +1,38 @@
+<?php
+// These attributes mimic those of Azure AD.
+$test_user_base = array(
+    'http://schemas.microsoft.com/identity/claims/tenantid' => 'ab4f07dc-b661-48a3-a173-d0103d6981b2',
+    'http://schemas.microsoft.com/identity/claims/objectidentifier' => '',
+    'http://schemas.microsoft.com/identity/claims/displayname' => '',
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/groups' => array(),
+    'http://schemas.microsoft.com/identity/claims/identityprovider' => 'https://sts.windows.net/da2a1472-abd3-47c9-95a4-4a0068312122/',
+    'http://schemas.microsoft.com/claims/authnmethodsreferences' => array('http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/password', 'http://schemas.microsoft.com/claims/multipleauthn'),
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => '',
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => '',
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => '',
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name' => ''
+);
+$config = array(
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'alice:password' => array_merge($test_user_base, array(
+            'http://schemas.microsoft.com/identity/claims/objectidentifier' => 'f2d75402-e1ae-40fe-8cc9-98ca1ab9cd5e',
+            'http://schemas.microsoft.com/identity/claims/displayname' => 'Alice Wonder',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'alice@acme.fr',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Alice',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Wonder',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name' => 'alice@acme.fr'
+        )),
+        'bob:password' => array_merge($test_user_base, array(
+            'http://schemas.microsoft.com/identity/claims/objectidentifier' => 'f2a94916-2fcb-4b68-9eb1-5436309006a3',
+            'http://schemas.microsoft.com/identity/claims/displayname' => 'Bob Moran',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'bob@acme.fr',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Bob',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Moran',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name' => 'bob@acme.fr'
+        )),
+    ),
+);
